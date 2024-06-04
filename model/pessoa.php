e751a1b214631846a1ef2872d736441bc4660531
LuanLancioni <luanlancioni12@gmail.com>
2024-06-04 12:14:36 -0300

    comentários

--- model/pessoa.php
+++ model/pessoa.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/projeto_web_banco_de_dados/controller/conexao.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/exemplo_banco_de_dados/controller/conexao.php';
+//indica onde este arquivo irá buscar informações em outra pasta no arquivo conexao.php
 
 class Pessoa{
     private $id;
@@ -13,6 +14,7 @@ class Pessoa{
     private $telefone;
     private $celular;
     private $conexao;
+    //cria as variáveis para receber informações do usuário do arquivo index.php que apresenta uma tela de cadastro
 
     public function getId() {
         return $this->id;
@@ -76,29 +78,42 @@ class Pessoa{
     public function setCelular($celular) {
         $this->celular = $celular;
     }
+    /*no geral eles todos os get e set servem para receber a informação correspondente 
+    ex: getCelular e setCelular pegam a informação do celular e guardam na variável $celular*/
 
 
     public function __construct() {
         $this->conexao = new Conexao();
     }
+    //intância a classe conexão
 
     public function inserir() {
         $sql = "INSERT INTO pessoa (`nome`, `endereco`, `bairro`, `cep`, `cidade`, `estado`, `telefone`, `celular`) VALUES (?,?,?,?,?,?,?,?)";
+        //utiliza o comando INSERT do sql para inserir dados no banco de dados, mas deixa os valores vazios
         $stmt = $this->conexao->getConexao()->prepare($sql);
+        //a partir de conexao que é um objeto de Conexao, ele usa a função getConexao e prepare para de fato inserir os dados
         $stmt->bind_param('ssssssss', $this->nome, $this->endereco, $this->bairro, $this->cep, $this->cidade, $this->estado, $this->telefone, $this->celular);
+        //nesta linha os valores são de fato inseridos
         return $stmt->execute();
+        //retorna a execução dos comandos
     }
 
     public function listar() {
         $sql = "SELECT * FROM pessoa";
+        //utiliza o comando SELECT para exibir as informações
         $stmt = $this->conexao->getConexao()->prepare($sql);
+        //a partir de conexao que é um objeto de Conexao, ele usa a função getConexao e prepare para de fato exibir os dados
         $stmt->execute();
+        //executa os comandos
         $result = $stmt->get_result();
+        //recebe os resultados dos comandos, ou seja as informações do usuário
         $pessoas = [];
         while($pessoa = $result->fetch_assoc()) {
             $pessoas[] = $pessoa;
+            //repete esse processo para pessoas diferentes e armazena em um vetor
         }
         return $pessoas;
+        //retorna as informações das pessoas cadastradas
     }
 }
 
